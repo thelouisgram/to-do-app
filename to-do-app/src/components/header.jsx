db02aa09bda11bedd8d0ab3ae34f9de59b6e5ee1
thelouisgram <hadesanoye01@gmail.com>
2023-04-08 23:23:56 +0100

    adjusted margins

--- to-do-app/src/components/header.jsx
+++ to-do-app/src/components/header.jsx
@@ -4,7 +4,7 @@ import { moon, sun } from '../assets';
 const Header = ({ mode, toggleMode }) => {
 	return (
 		// Header Container
-		<div className={`flex flex-row justify-between w-full items-center mt-[30px] mb-[30px] ss:w-[520px] `}>
+		<div className={`flex flex-row justify-between w-full items-center mt-[40px] mb-40px] ss:w-[520px] `}>
 			<h1 className={`font-josefin font-[700] text-[30px] tracking-[16px] text-white`}>TODO</h1>
 			<div onClick={toggleMode} className="cursor-pointer">
 				<img className="w-[20px] h-[20px]" src={mode === 'light' ? moon : sun} alt="mode" />
