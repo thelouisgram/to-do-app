51994fa02887de93a09c903cacf7cf2ccb5fab07
thelouisgram <hadesanoye01@gmail.com>
2023-04-13 10:14:22 +0100

    responsiveness

--- to-do-app/src/components/Notification.jsx
+++ to-do-app/src/components/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Notification = ({ currentMode, notificationMessage }) => {
     return (
         // Notification Container
-        <div className={`w-full h-auto py-[4px] fixed top-0 left-0 notification  font-josefin 
+        <div className={`w-full h-auto py-[4px] px-4 fixed top-auto notification  font-josefin 
         text-[16px] ${currentMode.background}  ${currentMode.text}`}>
             {/*  Notification Content */}
             <p className={`flex items-center justify-center `}>{notificationMessage}</p>
